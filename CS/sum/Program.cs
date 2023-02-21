74ed784b357e5f12f777034622d07006c1331a7c
imaarov <imanatarof@gmail.com>
2023-02-21 18:44:14 +0330

    add simple sum

---
+++ CS/sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter num1:");
+            string strInp1 = Console.ReadLine();
+
+            Console.WriteLine("Enter num2:");
+            string strInp2 = Console.ReadLine();
+
+            int intInp1 = Convert.ToInt32(strInp1);
+            int intInp2 = Convert.ToInt32(strInp2);
+
+            Console.WriteLine("Sum (integer) : " + (intInp1 + intInp2));
+            Console.WriteLine("Sum (string) : " + (strInp1 + strInp2));
+
+        }
+    }
+}
