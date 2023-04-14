c549a866e087c97dfc64a9db27fe7f70ae797717
imaarov <imanatarof@gmail.com>
2023-04-14 16:15:20 +0330

    add box volume calc app

---
+++ CS/VolBox/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter width: ");
+            int w = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter length: ");
+            int l = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter height: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+            double res = calc(w, l, h);
+            Console.WriteLine("Vol is " + res);
+        }
+
+        static private double calc(int w, int l, int h)
+        {
+            return w * l * h;
+        }  
+    }
+}
--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Collection of university homework.
 
 ## List:
 - [sum of two numbers (string & integer) (جمع دو عدد رشته ای و عددی)](CS/sum/)
-- [Duplicate by column c++ Way (گرفتن عدد تکراری با پیمایش ستونی)](CPP/ArrayColumnIterator/)
+- [duplicate by column c++ Way (گرفتن عدد تکراری با پیمایش ستونی)](CPP/ArrayColumnIterator/)
 - [determinant calculator (دترمینان با cpp)](CPP/Determinant/)
 - [determinant calculator C# Way (دترمینان با C#)](CS/Determinant)
 - [duplicate by column C# Way (گرفتن عدد تکراری با پیمایش ستونی با C#)](CS/Duplicate)
+- [box volume calc (محاسبه حجم جعبه c#)](CS/VolBox)
